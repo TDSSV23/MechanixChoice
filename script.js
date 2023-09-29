5ee6fdabdfa8a655bf8f8e7a39563adfc54f539f
felipehmazotti <126676451+felipehmazotti@users.noreply.github.com>
2023-09-29 08:03:07 -0300

    commit

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+// INICIO BUTTON SIDEBAR
+
+// OPACIDADE AO CLICAR NO BUTTON
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
-  
-  function closeNav() {
+// FIM DO EFEITO DE OPACIDADE NO BUTTON
+ 
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
     document.body.style.backgroundColor = "white";
   }
+  // FIM BUTTON SIDEBAR
