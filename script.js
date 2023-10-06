af9c7337e1d313247502c295b2b8ff68d1dc759d
felipehmazotti <126676451+felipehmazotti@users.noreply.github.com>
2023-10-05 22:54:59 -0300

    commit

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
   }
   // FIM BUTTON SIDEBAR
+
+  // Obtém o botão "Abrir" e a janela modal
+var openButton = document.getElementById("openModal");
+var modal = document.getElementById("myModal");
+
+// Quando o botão "Abrir" é clicado, exibe a janela modal
+openButton.addEventListener("click", function() {
+  modal.style.display = "block";
+});
+
+// Para fechar a janela modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Você pode adicionar um evento de clique fora da janela modal para fechá-la
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    closeModal();
+  }
+});
