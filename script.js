583687f465105460f62f896bcf999212b9876a4c
felipehmazotti <126676451+felipehmazotti@users.noreply.github.com>
2023-11-01 15:38:28 -0300

    commit

--- script.js
+++ script.js
@@ -36,3 +36,29 @@ const myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
+
+// tab
+
+var OpenTab;
+
+function showTab(num) {
+  try{
+      if(OpenTab!=undefined){
+        var OldTabID = document.getElementById('tab'+OpenTab);
+        var OldPanelID = document.getElementById('panel'+OpenTab);
+        OldTabID.className = '';
+        OldPanelID.className = 'hide';
+        OldTabID.setAttribute('aria-selected', false);
+        OldPanelID.setAttribute('aria-hidden', true);
+      }
+      var TabID = document.getElementById('tab'+num);
+      var PanelID = document.getElementById('panel'+num);
+      TabID.className = 'selected';
+      PanelID.className = 'selected';
+      TabID.setAttribute('aria-selected', true);
+      PanelID.setAttribute('aria-hidden', false);
+      OpenTab = num;
+  }catch(e){}
+}
+
+showTab(1);
